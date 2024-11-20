d4a50f3dc5ad2304b5d2c1f90f2c060c1f1cae0c
Nikhila C <115739037+Nikhilaniki@users.noreply.github.com>
2024-11-20 22:08:39 +0530

    logo

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         {/* Logo Section */}
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <div className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full flex items-center justify-center">
-            <Globe className="w-6 h-6" />
+          <div className="w-10 h-10 text-white p-2 bg-black rounded-full flex items-center justify-center">
+            <img src="https://t4.ftcdn.net/jpg/08/96/36/61/360_F_896366120_VbUMjA4KtfagqELPYdnkM7dlqKRPg90y.jpg" className="w-6 h-6" />
           </div>
           <span
             className="ml-3 text-xl font-black"
